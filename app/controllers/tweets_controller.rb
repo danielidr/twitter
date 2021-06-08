1b6c3edbaf998261a67f13aaabc44faee1e28ba2
Danielid Ramirez <danielid132@gmail.com>
2021-06-08 00:02:27 -0400

    Adding tweets to home

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,7 +22,7 @@ class TweetsController < ApplicationController
   # POST /tweets or /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
-
+    @tweet.user_id = current_user.id
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
