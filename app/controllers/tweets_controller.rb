b89c330c1521948031df00d86cc371b0702daa79
Danielid Ramirez <danielid132@gmail.com>
2021-06-11 20:12:52 -0400

    Modifying tweet show

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -65,6 +65,6 @@ class TweetsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def tweet_params
-      params.require(:tweet).permit(:content, :user_id, :origin_id, :is_rt)
+      params.require(:tweet).permit(:content, :user_id, :origin_id, :is_rt, :name, :profile_picture)
     end
 end
