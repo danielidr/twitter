6fe9330fd3f17f23c1c0771a05a2f8ad0c6ec24e
Danielid Ramirez <danielid132@gmail.com>
2021-06-17 22:59:02 -0400

    Adding search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,29 @@
 class HomeController < ApplicationController
 
   def index
-    if current_user
+    if current_user 
       friends_id = Friend.ids_friends(current_user.id)
-      @tweets = Tweet.tweets_for_me(friends_id).order(created_at: :DESC).page(params[:page])
+      friends_tweets = Tweet.tweets_for_me(friends_id).order(created_at: :DESC).page(params[:page])
+      @tweets = friends_tweets
+      if params[:q]
+        @tweets = @tweets.where('content ILIKE ?', "%#{params[:q]}%")
+        if @tweets.nil?
+          @tweets = friends_tweets
+        end
+      else
+        @tweets = friends_tweets
+      end
     else
-      @tweets = Tweet.all.order(created_at: :DESC).page(params[:page])
+      all_tweets = Tweet.all.order(created_at: :DESC).page(params[:page])
+      @tweets = all_tweets
+      if params[:q]
+        @tweets = @tweets.where('content ILIKE ?', "%#{params[:q]}%")
+        if @tweets.nil?
+          @tweets = all_tweets
+        end
+      else
+        @tweets = all_tweets
+      end
     end
     @tweet = Tweet.new
   end
